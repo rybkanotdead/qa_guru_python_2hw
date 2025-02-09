21039a0822ddf0c7eb26a847fe4dd745bf86e7ea
Alina Oganina <ifoxspirit@yandex.ru>
2025-02-09 15:04:24 +0500

    Remove .idea folder from repository

--- test_hw2.py
+++ test_hw2.py
@@ -8,17 +8,18 @@ from time import sleep
 def browser_conf():
     browser.config.window_height = 1020
     browser.config.window_width = 1080
-    browser.open('https://www.google.com/')
     yield
     browser.quit()
 
 
 def test_suc_search():
+    browser.open('https://www.google.com/')
     browser.element('[name="q"]').should(be.blank).type('kazakhstan').press_enter()
     sleep(10)
     browser.element('html').should(have.text('Страна, Центральная Азия'))
 
 def test_unsuc_search():
+    browser.open('https://www.google.com/')
     browser.element('[name="q"]').should(be.blank).type('asjdadffasfag').press_enter()
     sleep(10)
     browser.element('html').should(have.text('ничего не найдено'))
